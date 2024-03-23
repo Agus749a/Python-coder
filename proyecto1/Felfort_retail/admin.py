7afd6d35f7082a59d3a7b142c9928b7423632da0
Agustin Pacheco <wpacheco@frba.utn.edu.ar>
2024-03-23 13:40:50 -0300

    Delete proyecto1 directory

--- proyecto1/Felfort_retail/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import clientes
-from .models import productos
-
-admin.site.register(clientes)
-admin.site.register(productos)
--- proyecto1/Felfort_retail/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FelfortRetailConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Felfort_retail'
